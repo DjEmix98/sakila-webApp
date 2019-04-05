bc2ba6cacf0e973a09b42c9cbb862f9157c4e851
Emmanuel Bellissimo <emmanuel.bellissimo@objectmethod.it>
2019-04-05 14:32:30 +0200

    Modificato microservizio senza csvwriter e aggiunto microservizio con csvwriter

--- src/main/java/it/objectmethod/sakila/sakila/controller/rest/ActorControllerRest.java
+++ src/main/java/it/objectmethod/sakila/sakila/controller/rest/ActorControllerRest.java
@@ -1,16 +1,25 @@
 package it.objectmethod.sakila.sakila.controller.rest;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.validator.internal.util.privilegedactions.NewSchema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.opencsv.CSVWriter;
 
 import it.objectmethod.sakila.sakila.entity.Actor;
 import it.objectmethod.sakila.sakila.file.ByteWrite;
@@ -32,26 +41,39 @@ public class ActorControllerRest {
 		return attoriRepo.getOne(id);
 	} 
 
-	@GetMapping("/{idFilm}/attori-csv")
-	public OutputStream attoriFileCsv(@PathVariable("idFilm") Long idFilm, HttpServletResponse response) throws Exception{
-
+/*	@GetMapping("/{idFilm}/attori-csv") //senza CSVWriter
+	public void attoriFileCsv(@PathVariable("idFilm") Long idFilm, HttpServletResponse response) throws Exception{
+		
 		OutputStream os = response.getOutputStream();
-		byte[] csvArray = atorSerializer.attoriToCSV(attoriRepo.findByFilmId(idFilm), os);
 		List<Actor> attori = attoriRepo.findByFilmId(idFilm);
-		response.setContentType("text/csv");
-		response.setHeader("Content-Disposition", "filename=Attori.csv");
-		response.setContentLength(csvArray.length);
 		for(Actor a: attori) {
 			String attore = a.getNome()+";"+a.getCognome()+";";
 			os.write(attore.getBytes());
 		}
-//		os.write(csvArray, 0, csvArray.length);
-		
-		return os;
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "filename=Attori.csv");
+	}*/
 
+	@GetMapping("/{idFilm}/attori-csv") //con CSVWriter
+	public void actorFileCsv(@PathVariable("idFilm") Long idFilm, HttpServletResponse response) throws Exception{
+	
+		OutputStream os = response.getOutputStream();
+		CSVWriter writer = new CSVWriter( new OutputStreamWriter(os), CSVWriter.DEFAULT_SEPARATOR,
+                CSVWriter.NO_QUOTE_CHARACTER,
+                CSVWriter.NO_ESCAPE_CHARACTER,
+                CSVWriter.DEFAULT_LINE_END);
+		List<Actor> listAttori = attoriRepo.findByFilmId(idFilm);
+		for(Actor a: listAttori) {
+			System.out.print(a.getNome());
+			String[] myArray = {a.getNome()+";"+a.getCognome()+";"};
+		    writer.writeNext(myArray);
+		}
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "filename=Attori.csv");
+		writer.close();
+		 
+		 
 	}
-
-
 
 
 }
